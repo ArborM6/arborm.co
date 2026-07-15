80d7011f9694ff4bd6c4f20b213e7f3cd29823a2
yoko <6catxyz@gmail.com>
2026-07-15 23:33:27 +0800

    style: version homepage hero image

--- app/HomeContent.tsx
+++ app/HomeContent.tsx
@@ -206,10 +206,10 @@ export default function HomeContent({ initialLang }: { initialLang: LangKey }) {
 
       {/* Hero */}
       <section className="pt-14 md:pt-16">
-        <div className="w-full aspect-[211/90] relative">
+        <div className="w-full aspect-[1688/627] relative">
           <div
             className="absolute inset-0 bg-contain bg-center bg-no-repeat grayscale"
-            style={{ backgroundImage: "url('/arborm-hero.jpg')" }}
+            style={{ backgroundImage: "url('/arborm-hero-20260715.jpg')" }}
           />
         </div>
         <div className="max-w-5xl mx-auto w-full flex justify-end px-6 md:px-12 pt-8 pb-4">
